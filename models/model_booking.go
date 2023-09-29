da097050cd47231f3a8c251538ae1b5a07879958
MrWildanMD <mrwildanm7@gmail.com>
2023-09-30 05:14:50 +0700

    fix silly mistake

--- models/model_booking.go
+++ models/model_booking.go
@@ -7,18 +7,18 @@ import (
 )
 
 type Booking struct {
-    gorm.Model
-    UserID         int       `gorm:"type:integer;not null" json:"user_id"`
-    RoomID         int       `gorm:"type:integer;not null" json:"room_id"`
-    CheckIn        time.Time `gorm:"type:datetime;not null" json:"check_in"`
-    CheckOut       time.Time `gorm:"type:datetime;not null" json:"check_out"`
-    NumberOfGuests int       `gorm:"type:integer;not null" json:"number_of_guests"`
-    BookingStatus  int       `gorm:"type:integer;not null" json:"booking_status"`
-    AdditionalItem string    `gorm:"type:text" json:"additional_item"`
-    ApprovalID     int       `json:"approval_id"`
-    User           Users      `gorm:"foreignkey:UserID" json:"user"`
-    Room           Room      `gorm:"foreignkey:RoomID" json:"room"`
-    Notifications  []Notification `gorm:"foreignkey:BookingID" json:"notifications"`
+	gorm.Model
+	UserID         int            `gorm:"type:integer;not null" json:"user_id"`
+	RoomID         int            `gorm:"type:integer;not null" json:"room_id"`
+	CheckIn        time.Time      `gorm:"not null" json:"check_in"`
+	CheckOut       time.Time      `gorm:"not null" json:"check_out"`
+	NumberOfGuests int            `gorm:"type:integer;not null" json:"number_of_guests"`
+	BookingStatus  int            `gorm:"type:integer;not null" json:"booking_status"`
+	AdditionalItem string         `gorm:"type:text" json:"additional_item"`
+	ApprovalID     int            `json:"approval_id"`
+	User           Users          `gorm:"foreignkey:UserID" json:"user"`
+	Room           Room           `gorm:"foreignkey:RoomID" json:"room"`
+	Notifications  []Notification `gorm:"foreignkey:BookingID" json:"notifications"`
 }
 
 func (b *Booking) TableName() string {
--- models/model_notification.go
+++ models/model_notification.go
@@ -1,20 +1,17 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Notification struct {
-    gorm.Model
-    UserID    int       `gorm:"type:integer;not null" json:"user_id"`
-    BookingID int       `gorm:"type:integer" json:"booking_id"`
-    Content   string    `gorm:"type:varchar(250);not null" json:"content"`
-    Status    int       `gorm:"type:integer;not null" json:"status"`
-    Timestamp time.Time `gorm:"type:datetime;not null" json:"timestamp"`
-    User      Users      `gorm:"foreignkey:UserID" json:"user"`
-    Booking   Booking   `gorm:"foreignkey:BookingID" json:"booking"`
+	gorm.Model
+	UserID    int     `gorm:"type:integer;not null" json:"user_id"`
+	BookingID int     `gorm:"type:integer" json:"booking_id"`
+	Content   string  `gorm:"type:varchar(250);not null" json:"content"`
+	Status    int     `gorm:"type:integer;not null" json:"status"`
+	User      Users   `gorm:"foreignkey:UserID" json:"user"`
+	Booking   Booking `gorm:"foreignkey:BookingID" json:"booking"`
 }
 
 func (n *Notification) TableName() string {
